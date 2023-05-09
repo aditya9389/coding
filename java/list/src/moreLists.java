d5e0b265965820a1ce15b579182da889773d7776
aditya9389 <100075842+aditya9389@users.noreply.github.com>
2023-05-10 00:04:01 +0530

    java

--- java/list/src/moreLists.java
+++ java/list/src/moreLists.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class moreLists {
+    public static void main(String[] args)
+    {
+        String[] items ={"apples", "bananas"};
+        List<String> list = List.of(items);
+        System.out.println(list);       //imutable cant add new atime
+        ArrayList<String> groceries = new ArrayList<>(list);
+        groceries.add("yogrut");    //new item can be add in arratlist
+        System.out.println(groceries);
+        ArrayList<String> nextlist = new ArrayList<>(
+                List.of("picles","mustard"));
+        System.out.println(nextlist);
+        groceries.addAll(nextlist);
+        System.out.println(groceries);
+    }
 }
